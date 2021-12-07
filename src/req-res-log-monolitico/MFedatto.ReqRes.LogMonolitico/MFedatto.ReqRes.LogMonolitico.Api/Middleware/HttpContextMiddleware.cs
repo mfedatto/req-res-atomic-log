3d0243948ffc4d9dd4232c697c4664f426233670
Maurício Fedatto <mfedatto@gmail.com>
2021-12-06 21:10:27 -0300

    http context middleware;

---
+++ src/req-res-log-monolitico/MFedatto.ReqRes.LogMonolitico/MFedatto.ReqRes.LogMonolitico.Api/Middleware/HttpContextMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace MFedatto.ReqRes.LogMonolitico.Api.Middleware
+{
+    public class HttpContextMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public HttpContextMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+        }
+    }
+}
--- src/req-res-log-monolitico/MFedatto.ReqRes.LogMonolitico/MFedatto.ReqRes.LogMonolitico.Api/Program.cs
+++ src/req-res-log-monolitico/MFedatto.ReqRes.LogMonolitico/MFedatto.ReqRes.LogMonolitico.Api/Program.cs
@@ -1,3 +1,5 @@
+using MFedatto.ReqRes.LogMonolitico.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<HttpContextMiddleware>();
 
 app.Run();
